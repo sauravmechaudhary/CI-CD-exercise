f27a74cf4ebe52b97932848dc9090384dacd81fd
SauravChaudhary <135222864+sauravmechaudhary@users.noreply.github.com>
2026-05-10 19:24:39 +0300

    fix: successfully isolated Jest and Playwright tests

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@playwright/test')
 
 module.exports = defineConfig({
   testDir: './src',
-  testMatch: 'pokedex.spec.js',
+  testMatch: '**/*.spec.js',
   use: {
     baseURL: 'http://localhost:3000'
   },
