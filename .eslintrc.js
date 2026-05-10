737b0bd9137e6685634877f05c363d5e3af31053
SauravChaudhary <135222864+sauravmechaudhary@users.noreply.github.com>
2026-05-10 17:18:20 +0300

    Fix ESLint config and add missing plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,11 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
-    'jest/globals': true,
+    'jest/globals': true
   },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
+    'plugin:react/recommended'
   ],
   parserOptions: {
     ecmaFeatures: {
@@ -17,35 +17,19 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: [
-    'react', 'jest',
+    'react', 'jest'
   ],
   rules: {
-    'indent': [
-      'error',
-      2,
-    ],
-    'linebreak-style': [
-      'error',
-      'unix',
-    ],
-    'quotes': [
-      'error',
-      'single',
-    ],
-    'semi': [
-      'error',
-      'never',
-    ],
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
     'eqeqeq': 'error',
     'no-trailing-spaces': 'error',
-    'object-curly-spacing': [
-      'error', 'always',
-    ],
-    'arrow-spacing': [
-      'error', { 'before': true, 'after': true },
-    ],
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { 'before': true, 'after': true }],
     'no-console': 'error',
-    'react/prop-types': 0,
+    'react/prop-types': 0
   },
   settings: {
     react: {
