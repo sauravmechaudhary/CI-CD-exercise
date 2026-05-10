49ea37eebf0a5d9da37b6dfc757a14723e3e5a32
SauravChaudhary <135222864+sauravmechaudhary@users.noreply.github.com>
2026-05-10 20:52:39 +0300

    Add deployment pipeline

--- playwright.config.js
+++ playwright.config.js
@@ -4,11 +4,15 @@ module.exports = defineConfig({
   testDir: './src',
   testMatch: '**/*.spec.js',
   use: {
-    baseURL: 'http://localhost:3000'
+    // Match the port defined in your index.js
+    baseURL: 'http://localhost:5001',
   },
   webServer: {
     command: 'npm start',
-    url: 'http://localhost:3000',
-    reuseExistingServer: !process.env.CI
+    // Match the port defined in your index.js
+    url: 'http://localhost:5001',
+    reuseExistingServer: !process.env.CI,
+    // Give CI a bit more time to start the server (120 seconds)
+    timeout: 120 * 1000,
   }
 })
