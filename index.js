0a8eafeb7321718b518a3a3a72b9d392f6d9cff8
SauravChaudhary <135222864+sauravmechaudhary@users.noreply.github.com>
2026-05-11 14:28:42 +0300

    fix: resolve 404 on health check

--- index.js
+++ index.js
@@ -21,3 +21,12 @@ app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
+
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+/*
+  Checiking if it works or not
+*/
